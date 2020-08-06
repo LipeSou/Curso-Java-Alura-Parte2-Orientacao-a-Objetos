fa11e249877d8d078499ecdb6a04367530ce3d07
Felipe Souto <68256101+LipeSou@users.noreply.github.com>
2020-08-05 23:30:22 -0300

    Add files via upload

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -3,4 +3,30 @@ public class Conta {
     int agencia;
     int numero; 
     String titular;
+    
+    public void deposita(double valor) {
+    	this.saldo = this.saldo + valor;
+    }
+    
+     public boolean saca(double valor){
+    	if(this.saldo >= valor){
+    		this.saldo = this.saldo - valor;
+    		return true;
+    	}else {
+    		System.err.println("vc nao tem dinheiro suficiente");
+    		return false;
+    	}
+    }
+     
+     public boolean transfere (double valor, Conta destino){
+    	 if(this.saldo >= valor) {
+    		 this.saldo -= valor;
+    		// podemos fazer assim  destino.saldo +=  valor; ou
+    		 destino.deposita(valor);
+    		 return true;
+    	 } else {
+    		 System.err.println("nao tem dinheiro");
+    		 return false;
+    	 }
+     }
 }
